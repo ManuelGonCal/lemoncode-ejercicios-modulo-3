0913a5df74756949aacd5e02a575422fbb927b4d
Manuel Gonzalez <manuelmaingonzalez@gus.global>
2021-01-26 13:35:26 +0100

    Img going through React now

--- webpack/src/helloWorldComponent.tsx
+++ webpack/src/helloWorldComponent.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import classes from './helloWorldStyles.scss';
+import logoImg from './assets/socio.jpg';
+
+const img = document.createElement('img');
+console.log(logoImg);
 
 export const HelloWorldComponent: React.FC = () => {
-  return <h1 className={classes.helloWorld}>Hello World From React</h1>;
+  return (
+    <>
+      <h1 className={classes.helloWorld}>Hello World From React</h1>
+      <div className={classes.imgContainer}>
+        <img src={logoImg} alt="" />
+      </div>
+    </>
+  );
 };
